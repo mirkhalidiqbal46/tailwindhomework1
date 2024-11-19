1672206d346040769398b21f5495c877389d8c93
mirkhalidiqbal46 <mirkhalidiqbal46@gmail.com>
2024-11-19 18:34:38 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // Include all HTML files in the root directory
+    "./src/**/*.{html,js}", // Include HTML and JS files in the `src` folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
